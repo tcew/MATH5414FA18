c9f68ebfc76d4aad753720b4b280e4fa0bfe0a55
Tim Warburton <tim.warburton@gmail.com>
2018-10-04 20:09:23 -0400

    added some comments

--- src/meshMain.c
+++ src/meshMain.c
@@ -11,8 +11,7 @@ int main(int argc, char **argv){
 
   meshHaloSetupTri2D(mesh);
 
-  int *qrank = (int*) calloc(mesh->Nelements + mesh->NhaloElements,
-		  sizeof(int));
+  int *qrank = (int*) calloc(mesh->Nelements + mesh->NhaloElements, sizeof(int));
 
   int rank, size;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -51,49 +50,45 @@ int main(int argc, char **argv){
   meshVolumeGeometricFactorsTri2D(mesh);
 
   // load polynomial data for reference triangle
-  int N = 5;
+  int N = 5; // use this for polynomial array
   meshLoadReferenceNodesTri2D(mesh, N);
-  
-  // insert code to initialize differentiation matrices here
-  mesh->Dr = (dfloat*) calloc(mesh->Np*mesh->Np, sizeof(dfloat));
-  mesh->Ds = (dfloat*) calloc(mesh->Np*mesh->Np, sizeof(dfloat));
-  
-  //
+
+  // initialize HOST arrays
   dfloat *q     = (dfloat*) calloc(  mesh->Np*(mesh->Nelements+mesh->NhaloElements), sizeof(dfloat));
   dfloat *gradq = (dfloat*) calloc(2*mesh->Np*(mesh->Nelements+mesh->NhaloElements), sizeof(dfloat));
 
+  // compute on HOST
   meshGradientTri2D(mesh, q, gradq);
 
-  occa::device device("mode: 'Serial'");
+  // initialize DEVICE
+  occa::device device("mode: 'CUDA', device_id: 0");
 
+  // allocate array space on DEVICE
   occa::memory o_q = device.malloc(mesh->Nelements*mesh->Np*sizeof(dfloat), q);
   occa::memory o_gradq = device.malloc(2*mesh->Nelements*mesh->Np*sizeof(dfloat), gradq);
-  occa::memory o_vgeo = device.malloc(mesh->Nelements*mesh->Nvgeo*sizeof(dfloat),
-				      mesh->vgeo);
 
-  occa::memory o_Dr = device.malloc(mesh->Np*mesh->Np*sizeof(dfloat),
-				    mesh->Dr);
-  
-  
-  occa::memory o_Ds = device.malloc(mesh->Np*mesh->Np*sizeof(dfloat),
-				      mesh->Ds);
+  occa::memory o_vgeo = device.malloc(mesh->Nelements*mesh->Nvgeo*sizeof(dfloat), mesh->vgeo);
 
+  occa::memory o_Dr = device.malloc(mesh->Np*mesh->Np*sizeof(dfloat), mesh->Dr);
+  occa::memory o_Ds = device.malloc(mesh->Np*mesh->Np*sizeof(dfloat), mesh->Ds);
+
+  // properties to be used for defining compiler variables
   occa::properties props;
-  props["defines/p_RXID"] = p_RXID;
-  props["defines/p_RYID"] = p_RYID;
-  props["defines/p_SXID"] = p_SXID;
-  props["defines/p_SYID"] = p_SYID;
-  props["defines/p_JID"] = p_JID;
+  props["defines/p_RXID"]  = p_RXID;
+  props["defines/p_RYID"]  = p_RYID;
+  props["defines/p_SXID"]  = p_SXID;
+  props["defines/p_SYID"]  = p_SYID;
+  props["defines/p_JID"]   = p_JID;
   props["defines/p_Nvgeo"] = mesh->Nvgeo;
-  props["defines/dfloat"] = dfloatString;
-  
-  occa::kernel gradientKernel
-    = device.buildKernel("src/meshGradientTri2D.okl",
-			 "meshGradientTri2D",
-			 props);
+  props["defines/dfloat"]  = dfloatString;
 
+  // build kernel on DEVICE
+  occa::kernel gradientKernel = device.buildKernel("src/meshGradientTri2D.okl", "meshGradientTri2D", props);
+
+  // compute on DEVICE
   gradientKernel(mesh->Nelements, mesh->Np, o_Dr, o_Ds, o_vgeo, o_q, o_gradq);
 
+  // copy data back to HOST
   o_q.copyTo(q);
   o_gradq.copyTo(gradq);
   
