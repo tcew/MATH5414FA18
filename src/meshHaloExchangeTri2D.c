8e9014b79e0342489c3552ce764a7dcd3c639ede
Tim Warburton <tim.warburton@gmail.com>
2018-10-16 17:06:41 -0400

    starting to split halo exchange apart

--- src/meshHaloExchangeTri2D.c
+++ src/meshHaloExchangeTri2D.c
@@ -67,3 +67,72 @@ void meshHaloExchangeTri2D(mesh_t *mesh,
   free(recvRequests);
   free(qout);
 }
+
+
+void meshHaloExchangeStartTri2D(mesh_t *mesh,
+				void   *q,
+				void   *qout,
+				int bytesPerElement,
+				MPI_Requests *sendRequests,
+				MPI_Requests *recvRequests){
+
+  int rank, size;
+
+  /* 
+     NhaloElements
+     haloElementIndices
+     NhaloExchangeElements (number to send per rank)
+  */
+  
+  int NhaloElements = mesh->NhaloElements;
+  
+  for(int h=0;h<NhaloElements;++h){
+    int e = mesh->haloElementIndices[h];
+    memcpy(qout+h*bytesPerElement,
+	   (unsigned char*) q+e*bytesPerElement,
+	   bytesPerElement);
+  }
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  unsigned char *qin = (unsigned char*) q + bytesPerElement*mesh->Nelements;
+  int cnt = 0;
+  for(int r=0;r<size;++r){
+    if(rank!=r){
+      int Nexchange =
+	bytesPerElement*mesh->NhaloExchangeElements[r];
+      if(Nexchange>0){
+	int tag = 999;
+	MPI_Isend(qout+cnt, Nexchange, MPI_CHAR,
+		  r, tag, MPI_COMM_WORLD, sendRequests+r);
+	MPI_Irecv(qin+cnt, Nexchange, MPI_CHAR,
+		  r, tag, MPI_COMM_WORLD, recvRequests+r);
+	cnt += Nexchange;
+      }
+    }
+  }
+}
+
+void meshHaloExchangeFinishTri2D(mesh_t *mesh,
+				 int bytesPerElement,
+				 MPI_Requests *sendRequests,
+				 MPI_Requests *recvRequests){
+
+  int rank,size;
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  
+  for(int r=0;r<size;++r){
+    if(rank!=r){
+      int Nexchange =
+	bytesPerElement*mesh->NhaloExchangeElements[r];
+      if(Nexchange>0){
+	MPI_Status status;
+	MPI_Wait(sendRequests+r, &status);
+	MPI_Wait(recvRequests+r, &status);
+      }
+    }
+  }
+}
